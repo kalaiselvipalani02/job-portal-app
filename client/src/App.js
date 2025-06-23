d2aa80d4d973c0908fd821a7d9a8d8554d24ba91
kalaiselvipalani02 <kalaiselvi.palani02@gmail.com>
2025-06-23 13:59:36 +0530

    Fix: add client folder as regular directory

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/") // Call backend
+      .then((res) => setMessage(res.data))
+      .catch((err) => console.error(err));
+  }, []);
+
+  return <div>Job Portal Web Application -{message}</div>;
+}
+
+export default App;
